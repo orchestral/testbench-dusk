a873c35904f590d8f69f9f1970857e11b99fbb19
Keoghan Litchfield <keoghan@klever.co.uk>
2018-01-13 22:24:45 +0000

    Add helper functions for waiting for servers to start/stop.

--- tests/Unit/DuskServerTest.php
+++ tests/Unit/DuskServerTest.php
@@ -34,13 +34,14 @@ class DuskServerTest extends TestCase
             $b = new DuskServer;
 
             $a->start();
-            sleep(1);
+            $this->waitForServerToStart();
 
             $b->start();
 
         } catch (UnableToStartServer $e) {
             $a->stop();
             $b->stop();
+            $this->waitForServerToStop();
             $this->assertTrue(true, 'New server was not created with the same host and port');
 
             return;
@@ -48,6 +49,8 @@ class DuskServerTest extends TestCase
 
         $a->stop();
         $b->stop();
+
+        $this->waitForServerToStop();
 
         $this->fail('A server existed but we tried to set a new one up anyway');
     }
@@ -65,13 +68,12 @@ class DuskServerTest extends TestCase
                 // So create a server and exit (to simluate a dd() or similar in a test)
                 // Once complete, the parent can check for the orpahn server.
                 (new DuskServer)->start();
-                sleep(1);
+                $this->waitForServerToStart();
                 exit();
                 break;
             default:
                 // @parent
                 pcntl_waitpid($pid, $status);
-                sleep(2);
                 try {
                     (new DuskServer)->start();
                     $this->assertTrue(true, 'We did not end up with an orphan server');
@@ -80,5 +82,43 @@ class DuskServerTest extends TestCase
                 }
                 break;
         }
+    }
+
+    protected function waitForServerToStart()
+    {
+        $i = 0;
+
+        while (! $this->isServerUp()) {
+            sleep(1);
+            $i++;
+
+            if($i >= 10) {
+                throw new \Exception('Waited too long for server to start.');
+            }
+        }
+    }
+
+    protected function waitForServerToStop()
+    {
+        $i = 0;
+
+        while ($this->isServerUp()) {
+            sleep(1);
+            $i++;
+
+            if ($i >= 10) {
+                throw new \Exception('Waited too long for server to stop.');
+            }
+        }
+    }
+
+    protected function isServerUp()
+    {
+        if ($socket = @fsockopen('127.0.0.1', 8000, $errorNumber = 0, $errorString = '', $timeout = 1)) {
+            fclose($socket);
+            return true;
+        }
+
+        return false;
     }
 }
