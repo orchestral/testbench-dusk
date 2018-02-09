b7d73721b3a44ff45314462afeb222526f63c901
Keoghan Litchfield <Keoghan@users.noreply.github.com>
2018-02-09 09:34:09 +0000

    Improve server management [3.5] (#10)
     - Be more cautious with using/creating Dusk server. Fail if a server (or process) already exists on the host/port we want to use.
     - Add a shutdown_function to handle stopping the server if the test is prematurely closed (e.g. dd() or exit()).
     - Swap to using the Symfony Process class for starting and stopping the separate server process. Allows us to remove ProcessUtils as well.
     - Add 'exec' to the process command, which allows a clean shutdown on Ubuntu (found through Travis). (Relevant bug report for Symfony here: symfony/symfony#23568)

--- tests/Unit/DuskServerTest.php
+++ tests/Unit/DuskServerTest.php
@@ -2,6 +2,7 @@
 
 namespace Orchestra\Testbench\Dusk\Tests\Unit;
 
+use Orchestra\Testbench\Dusk\Exceptions\UnableToStartServer;
 use PHPUnit\Framework\TestCase;
 use Orchestra\Testbench\Dusk\DuskServer;
 
@@ -23,5 +24,101 @@ class DuskServerTest extends TestCase
             '/dir/project/laravel/public',
             (new DuskServer())->laravelPublicPath('/dir/project/laravel/public')
         );
+    }
+
+    /** @test */
+    public function it_fails_when_there_is_a_server_on_the_host_and_port_already()
+    {
+        try {
+            $a = new DuskServer;
+            $b = new DuskServer;
+
+            $a->start();
+            $this->waitForServerToStart();
+
+            $b->start();
+
+        } catch (UnableToStartServer $e) {
+            $a->stop();
+
+            $this->waitForServerToStop();
+            $this->assertTrue(true, 'New server was not created with the same host and port');
+
+            return;
+        }
+
+        $a->stop();
+        $b->stop();
+
+        $this->waitForServerToStop();
+
+        $this->fail('A server existed but we tried to set a new one up anyway');
+    }
+
+    /** @test */
+    public function an_early_exit_does_not_leave_an_orphan_server()
+    {
+        switch ($pid = pcntl_fork()) {
+            case -1:
+                $this->fail('Process fork failed when testing for an orphan server');
+                break;
+            case 0:
+                // @child
+                // We have everything from the script so far available
+                // So create a server and exit (to simluate a dd() or similar in a test)
+                // Once complete, the parent can check for the orpahn server.
+                (new DuskServer)->start();
+                $this->waitForServerToStart();
+                exit();
+                break;
+            default:
+                // @parent
+                pcntl_waitpid($pid, $status);
+                try {
+                    (new DuskServer)->start();
+                    $this->assertTrue(true, 'We did not end up with an orphan server');
+                } catch (UnableToStartServer $e) {
+                    $this->fail('There was an orphan server. You\'ll need to remove it manually.');
+                }
+                break;
+        }
+    }
+
+    protected function waitForServerToStart()
+    {
+        $i = 0;
+
+        while (! $this->isServerUp()) {
+            sleep(1);
+            $i++;
+
+            if($i >= 30) {
+                throw new \Exception('Waited too long for server to start.');
+            }
+        }
+    }
+
+    protected function waitForServerToStop()
+    {
+        $i = 0;
+
+        while ($this->isServerUp()) {
+            sleep(1);
+            $i++;
+
+            if ($i >= 30) {
+                throw new \Exception('Waited too long for server to stop.');
+            }
+        }
+    }
+
+    protected function isServerUp()
+    {
+        if ($socket = @fsockopen('127.0.0.1', 8000, $errorNumber = 0, $errorString = '', $timeout = 1)) {
+            fclose($socket);
+            return true;
+        }
+
+        return false;
     }
 }
