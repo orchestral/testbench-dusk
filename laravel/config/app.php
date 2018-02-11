5e7a997df454a1e5898fe88000232fc69e13136a
Mior Muhammad Zaki <crynobone@gmail.com>
2018-02-11 14:01:08 +0800

    Working on testing with loginAs().
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

--- laravel/config/app.php
+++ laravel/config/app.php
@@ -162,6 +162,7 @@ return [
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
 
+        Laravel\Dusk\DuskServiceProvider::class,
     ],
 
     /*
---
+++ tests/Browser/AuthTest.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Orchestra\Testbench\Dusk\Tests\Browser;
+
+use Laravel\Dusk\Browser;
+use Illuminate\Foundation\Auth\User;
+use Orchestra\Testbench\Dusk\TestCase;
+
+class AuthTest extends TestCase
+{
+    protected function setUp()
+    {
+        parent::setUp();
+
+        $this->withFactories(__DIR__.'/../factories');
+        $this->loadLaravelMigrations('sqlite');
+    }
+
+    /**
+     * Define environment setup.
+     *
+     * @param  \Illuminate\Foundation\Application  $app
+     *
+     * @return void
+     */
+    protected function getEnvironmentSetUp($app)
+    {
+        //
+    }
+
+    /** @test */
+    public function can_authenticate_user()
+    {
+        $user = factory(User::class)->create();
+
+        $this->browse(function (Browser $browser) use ($user) {
+            $browser->loginAs($user, 'web')
+                ->assertAuthenticatedAs($user, 'web');
+        });
+    }
+}
