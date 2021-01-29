b977ac1632ebf3f84880331aa8c31ff1db82c0db
Mior Muhammad Zaki <crynobone@gmail.com>
2021-01-29 18:07:05 +0800

    wip
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -5,4 +5,8 @@ include __DIR__.'/../vendor/autoload.php';
 // Example usage to set Dusk to run without UI.
 // You can enable UI with DuskOptions::withUI();
 
-Orchestra\Testbench\Dusk\Options::withoutUI();
+if (isset($_SERVER['CI']) || isset($_ENV['CI'])) {
+    Orchestra\Testbench\Dusk\Options::withoutUI();
+} else {
+    Orchestra\Testbench\Dusk\Options::withUI();
+}
