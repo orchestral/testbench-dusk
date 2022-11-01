08555cf14e3df6f588bd3ab7f84ed37fe8745584
Mior Muhammad Zaki <crynobone@gmail.com>
2022-11-01 14:33:28 +0800

    wip
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

--- src/Foundation/Console/ServeCommand.php
+++ src/Foundation/Console/ServeCommand.php
@@ -9,24 +9,9 @@ use Orchestra\Testbench\Foundation\Console\ServeCommand as Command;
 class ServeCommand extends Command
 {
     /**
-     * Copy the ".env" file.
+     * The environment file name.
      *
-     * @param  \Illuminate\Filesystem\Filesystem  $filesystem
-     * @param  string  $workingPath
-     * @return void
+     * @var string
      */
-    protected function copyTestbenchDotEnvFile(Filesystem $filesystem, string $workingPath): void
-    {
-        $configurationFile = Collection::make([
-            '.env.dusk',
-            '.env.dusk.example',
-            '.env.dusk.dist',
-        ])->map(fn ($file) => "{$workingPath}/{$file}")
-        ->filter(fn ($file) => $filesystem->exists($file))
-        ->first();
-
-        if (! is_null($configurationFile)) {
-            $filesystem->copy($configurationFile, $this->laravel->basePath('.env'));
-        }
-    }
+    protected $environmentFile = '.env.dusk';
 }
