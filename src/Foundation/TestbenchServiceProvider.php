58e45e1b67ea5b2599c333f659d8b1c79b22a765
Mior Muhammad Zaki <crynobone@gmail.com>
2022-11-06 21:33:57 +0800

    wip
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

--- src/Foundation/TestbenchServiceProvider.php
+++ src/Foundation/TestbenchServiceProvider.php
@@ -2,10 +2,29 @@
 
 namespace Orchestra\Testbench\Dusk\Foundation;
 
+use Composer\InstalledVersions;
+use Illuminate\Foundation\Console\AboutCommand;
 use Illuminate\Support\ServiceProvider;
 
 class TestbenchServiceProvider extends ServiceProvider
 {
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        /** @phpstan-ignore-next-line */
+        $workingPath = TESTBENCH_WORKING_PATH;
+
+        AboutCommand::add('Testbench', fn () => [
+            'Dusk Version' => class_exists(InstalledVersions::class) ? InstalledVersions::getPrettyVersion('orchestra/testbench-dusk') : '<fg=yellow;options=bold>-</>',
+            'Core Version' => class_exists(InstalledVersions::class) ? InstalledVersions::getPrettyVersion('orchestra/testbench-core') : '<fg=yellow;options=bold>-</>',
+            'Skeleton Path' => str_replace($workingPath, '', $this->app->basePath()),
+        ]);
+    }
+
     /**
      * Bootstrap services.
      *
