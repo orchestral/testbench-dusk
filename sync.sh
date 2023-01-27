e3f284561add82d49af98d841ea36d2f888796c1
Mior Muhammad Zaki <crynobone@gmail.com>
2023-01-27 21:29:14 +0800

    wip
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

--- sync.sh
+++ sync.sh
@@ -1,16 +1,20 @@
 #!/bin/bash
 
-cp -f vendor/laravel/laravel/.env.example laravel/
-cp -rf vendor/laravel/laravel/config/*.php laravel/config/
-cp -rf vendor/laravel/laravel/database/migrations/2014_10_12_000000_create_users_table.php laravel/migrations/2014_10_12_000000_testbench_create_users_table.php
-cp -rf vendor/laravel/laravel/database/migrations/2014_10_12_100000_create_password_resets_table.php laravel/migrations/2014_10_12_100000_testbench_create_password_resets_table.php
-cp -rf vendor/laravel/laravel/database/migrations/2019_08_19_000000_create_failed_jobs_table.php laravel/migrations/2019_08_19_000000_testbench_create_failed_jobs_table.php
-cp -rf vendor/laravel/laravel/lang/en/*.php laravel/lang/en/
-cp -rf vendor/laravel/laravel/resources/views/* laravel/resources/views/
+composer create-project "laravel/laravel:9.x-dev" skeleton --no-scripts --no-plugins --quiet
+
+cp -f skeleton/.env.example laravel/
+cp -rf skeleton/config/*.php laravel/config/
+cp -rf skeleton/database/migrations/2014_10_12_000000_create_users_table.php laravel/migrations/2014_10_12_000000_testbench_create_users_table.php
+cp -rf skeleton/database/migrations/2014_10_12_100000_create_password_resets_table.php laravel/migrations/2014_10_12_100000_testbench_create_password_resets_table.php
+cp -rf skeleton/database/migrations/2019_08_19_000000_create_failed_jobs_table.php laravel/migrations/2019_08_19_000000_testbench_create_failed_jobs_table.php
+cp -rf skeleton/lang/en/*.php laravel/lang/en/
+cp -rf skeleton/resources/views/* laravel/resources/views/
 cp -rf vendor/orchestra/testbench-core/laravel/server.php laravel/server.php
-cp -rf vendor/laravel/laravel/public/index.php laravel/public/index.php
+cp -rf skeleton/public/index.php laravel/public/index.php
 cp -rf vendor/laravel/dusk/stubs/phpunit.xml stubs/phpunit.xml
 rm laravel/config/sanctum.php
+
+rm -Rf skeleton
 
 awk '{sub(/DB_CONNECTION=mysql/,"DB_CONNECTION=sqlite")}1' laravel/.env.example > laravel/.env.example.stub && mv laravel/.env.example.stub laravel/.env.example
 awk '{sub(/DB_HOST=/,"\# DB_HOST=")}1' laravel/.env.example > laravel/.env.example.stub && mv laravel/.env.example.stub laravel/.env.example
