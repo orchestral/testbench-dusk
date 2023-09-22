c505fde0221f537e79a6b7c94107cc9b9a37b45b
Mior Muhammad Zaki <crynobone@gmail.com>
2023-09-22 12:56:04 +0800

    wip
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

--- tests/Browser/WorkbenchTest.php
+++ tests/Browser/WorkbenchTest.php
@@ -2,12 +2,27 @@
 
 namespace Orchestra\Testbench\Dusk\Tests\Browser;
 
+use Illuminate\Contracts\Http\Kernel as HttpKernel;
 use Orchestra\Testbench\Concerns\WithWorkbench;
 use Orchestra\Testbench\Dusk\TestCase;
+use Orchestra\Workbench\Http\Middleware\CatchDefaultRoute;
 
 class WorkbenchTest extends TestCase
 {
     use WithWorkbench;
+
+    /** @test */
+    public function it_can_browse_the_default_page()
+    {
+        $this->beforeServingApplication(function ($app) {
+            $app->make(HttpKernel::class)->pushMiddleware(CatchDefaultRoute::class);
+        });
+
+        $this->browse(function ($browser) {
+            $browser->visit('/')
+                ->assertSee('Laravel');
+        });
+    }
 
     /** @test */
     public function it_can_browse_the_welcome_page()
