176b7f2dd51e6feb133c794be3a5d4f5f022e5aa
Mior Muhammad Zaki <crynobone@gmail.com>
2023-11-28 18:47:59 +0800

    wip
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

---
+++ src/Attributes/BeforeServing.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Orchestra\Testbench\Dusk\Attributes;
+
+use Attribute;
+use Closure;
+use Orchestra\Testbench\Contracts\Attributes\Actionable as ActionableContract;
+
+#[Attribute(Attribute::TARGET_CLASS | Attribute::TARGET_METHOD)]
+final class BeforeServing implements ActionableContract
+{
+
+    /**
+     * The target method.
+     *
+     * @var string
+     */
+    public $method;
+
+    /**
+     * Construct a new attribute.
+     *
+     * @param  string  $method
+     */
+    public function __construct(string $method)
+    {
+        $this->method = $method;
+    }
+
+    /**
+     * Handle the attribute.
+     *
+     * @param  \Illuminate\Foundation\Application  $app
+     * @param  \Closure(string, array<int, mixed>):void  $action
+     */
+    public function handle($app, Closure $action): void
+    {
+        /** @var \Illuminate\Contracts\Config\Repository $config */
+        $config = $app->make('config');
+
+        \call_user_func($action, $this->method, [$app, $config]);
+    }
+}
