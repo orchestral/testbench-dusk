16cd4f6ed23cb64a04734705720e65a5d916e83f
Mior Muhammad Zaki <crynobone@gmail.com>
2024-01-09 06:44:20 +0800

    wip
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

--- src/Concerns/InteractsWithWebDriverOptions.php
+++ src/Concerns/InteractsWithWebDriverOptions.php
@@ -10,6 +10,8 @@ trait InteractsWithWebDriverOptions
      * Prepare the testing environment web driver options.
      *
      * @return void
+     *
+     * @codeCoverageIgnore
      */
     #[BeforeClass]
     public static function defineWebDriverOptions()
