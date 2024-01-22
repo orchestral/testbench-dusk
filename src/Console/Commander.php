6999684290785a84ba5d43ccba85a334b5dd2118
Mior Muhammad Zaki <crynobone@gmail.com>
2024-01-22 13:55:44 +0800

    wip
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

--- src/Console/Commander.php
+++ src/Console/Commander.php
@@ -2,12 +2,12 @@
 
 namespace Orchestra\Testbench\Dusk\Console;
 
-use Orchestra\Testbench\Console\Commander as Testbench;
+use Orchestra\Testbench\Dusk\Foundation\Application as Testbench;
 use Orchestra\Testbench\Dusk\Foundation\TestbenchServiceProvider;
 
 use function Orchestra\Testbench\Dusk\default_skeleton_path;
 
-class Commander extends Testbench
+class Commander extends \Orchestra\Testbench\Console\Commander
 {
     /**
      * The environment file name.
@@ -21,7 +21,7 @@ class Commander extends Testbench
      *
      * @var class-string<\Orchestra\Testbench\Foundation\Application>
      */
-    protected static string $testbench = \Orchestra\Testbench\Dusk\Foundation\Application::class;
+    protected static string $testbench = Testbench::class;
 
     /**
      * List of providers.
