3e54e290181c58d05b0bbdb436f3ea6d6220f9c4
Mior Muhammad Zaki <crynobone@gmail.com>
2024-10-26 16:42:22 +0800

    wip
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

--- src/DuskServer.php
+++ src/DuskServer.php
@@ -228,7 +228,7 @@ class DuskServer
     protected function prepareCommand(): array
     {
         return [
-            \function_exists(php_binary::class) ? php_binary() : (string) (new PhpExecutableFinder)->find(false),
+            \function_exists(php_binary::class) ? php_binary() : (string) (new PhpExecutableFinder)->find(false), // @phpstan-ignore class.notFound
             '-S',
             \sprintf('%s:%s', $this->host, $this->port),
             join_paths(__DIR__, 'server.php'),
