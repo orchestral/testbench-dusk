a019d21a3c79f4054d7c41048369bb542554cea4
Mior Muhammad Zaki <crynobone@gmail.com>
2024-11-20 15:31:07 +0800

    wip
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

--- src/Concerns/CanServeSite.php
+++ src/Concerns/CanServeSite.php
@@ -208,7 +208,7 @@ trait CanServeSite
      *
      * @codeCoverageIgnore
      */
-    #[\Deprecated('Use `createServingApplicationForDuskServer` instead', since: '6.40.0')]
+    #[\Deprecated('Use `createServingApplicationForDuskServer()` instead', since: '6.40.0')]
     public function getFreshApplicationToServe(DuskServer $server)
     {
         return $this->createServingApplicationForDuskServer($server);
