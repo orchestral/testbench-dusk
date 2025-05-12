9cec0b3c1e174352e0191b890fac2f0206533249
Mior Muhammad Zaki <crynobone@gmail.com>
2025-05-12 15:16:46 +0800

    wip
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

--- src/Concerns/CanServeSite.php
+++ src/Concerns/CanServeSite.php
@@ -171,7 +171,7 @@ trait CanServeSite
         /** @var \Illuminate\Foundation\Application $app */
         $app = $this->app;
 
-        $serializedClosure = unserialize(static::$server->getStash('tweakApplication'));
+        $serializedClosure = unserialize($server->getStash('tweakApplication'));
 
         if ($serializedClosure) {
             /** @var (\Closure(\Illuminate\Foundation\Application, \Illuminate\Contracts\Config\Repository):(void))|string $closure */
