0fc2a170f56cb120285e1d47e70fb40e06ce9531
Mior Muhammad Zaki <crynobone@gmail.com>
2025-06-08 19:11:49 +0800

    wip
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

--- src/server.php
+++ src/server.php
@@ -2,7 +2,7 @@
 
 use Orchestra\Testbench\Dusk\DuskServer;
 
-use function Orchestra\Testbench\join_paths;
+use function Orchestra\Sidekick\join_paths;
 
 // Simple server script, which pulls in large part from the framework.
 // It has been adapted so we can reconstruct the application to the
