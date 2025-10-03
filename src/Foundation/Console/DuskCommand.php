42e59989c8369c3df62db786dcf16398a0bac9c3
Mior Muhammad Zaki <crynobone@gmail.com>
2025-10-03 09:38:15 +0800

    Update orchestra/sidekick
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

--- src/Foundation/Console/DuskCommand.php
+++ src/Foundation/Console/DuskCommand.php
@@ -7,6 +7,7 @@ use Illuminate\Support\Str;
 use Laravel\Dusk\Console\DuskCommand as Command;
 use Symfony\Component\Console\Attribute\AsCommand;
 
+use function Orchestra\Sidekick\is_testbench_cli;
 use function Orchestra\Sidekick\join_paths;
 use function Orchestra\Testbench\package_path;
 
@@ -36,7 +37,7 @@ class DuskCommand extends Command
     {
         parent::configure();
 
-        if (! \defined('TESTBENCH_CORE')) {
+        if (! is_testbench_cli()) {
             $this->setHidden(true);
         }
     }
